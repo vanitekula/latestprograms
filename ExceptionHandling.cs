3434a37d5f582099e98d788627e5f49c4de79664
allamarunkumar-dotnet <78468146+allamarunkumar-dotnet@users.noreply.github.com>
2021-02-13 17:03:48 +0530

    Add files via upload

---
+++ ExceptionHandling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConsoleApp
+{
+    class Class5
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter Number1 : ");
+                    int num1 = int.Parse(Console.ReadLine());
+                    Console.Write("Enter Number2 : ");
+                    int num2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine(num1 + num2);
+                    Console.WriteLine(num1 / num2);
+                }
+                catch (FormatException fx)
+                {
+                    Console.WriteLine("Plz Enter only numbers");
+                }
+                catch(DivideByZeroException dx)
+                {
+                    Console.WriteLine("num2 can't be zero");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.GetType());
+                }
+              
+            }
+        }
+    }
+}
