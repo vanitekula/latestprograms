f0275d57dd684b4f65b8f4e01e9bc6f6a7f191c9
allamarunkumar-dotnet <78468146+allamarunkumar-dotnet@users.noreply.github.com>
2021-02-20 18:15:24 +0530

    Add files via upload

---
+++ NewRegisterPage.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication37
+{
+    public partial class NewRegisterPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string fname = TxtFname.Text;
+            string lname = TxtLname.Text;
+            string gen = RblListGender.SelectedItem.Text;
+            string hob = string.Empty;
+            for (int i = 0; i < CheBoxListHobbies.Items.Count; i++)
+            {
+                if (CheBoxListHobbies.Items[i].Selected)
+                {
+                    hob = hob + CheBoxListHobbies.Items[i].Text;
+                }
+            }
+            string con = TxtContact.Text;
+            string address = TxtAdress.Text;
+            string qual = DDLQualification.SelectedItem.Text;
+            LblResult.Text = string.Format($"Name :{fname}<br>LastName :{lname}<br>Gedner :{gen}<br>Hobbies : {hob}<br>Contact number :{con}<br>Qualification : {qual}<Br>Address : {address}");
+
+
+
+        }
+    }
+}
